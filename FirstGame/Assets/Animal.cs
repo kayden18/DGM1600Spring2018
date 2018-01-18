af70afc98bbb296891155d8fd646419395b865c5
Unknown <kayden18@ymail.com>
2018-01-18 11:14:49 -0700

    worked on animal class

---
+++ FirstGame/Assets/Animal.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Animal : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		print("My animal is eating.");
+		print("My animal is sleeping.");
+		print("I love my cat.");
+		print("If I had one!");
+	}
+	
+	void OnMouseDown()
+	{
+		print("Meow");
+	}
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
