bbd36685d1eb6a4517ed57e0b0d520147795a427
Unknown <kayden18@ymail.com>
2018-01-25 13:30:48 -0700

    variable homework

--- FirstGame/Assets/Animal2.cs
+++ FirstGame/Assets/Animal2.cs
@@ -4,12 +4,12 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class Animal2 : MonoBehaviour {
-
-	public string Name;
-	public int foodCount;
+//strings and ints are variables which is called during functions
+	public string Name; //string is a string of letters, numbers, and characters 
+	public int foodCount; //int is a whole number 
 	void OnMouseDown()
 	{
-		print("I have a " + Name);
-		print("It has " + foodCount + " amount of food.");
+		print("I have a " + Name);//strings can be called and combined with other strings through functions
+		print("It has " + foodCount + " amount of food.");//ints can also be called and combined with strings and other ints through functions
 	}
 }
