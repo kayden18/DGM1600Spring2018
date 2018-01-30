76c6cfaff7cad629188c8d986c875cc61cfa9540
Unknown <kayden18@ymail.com>
2018-01-30 11:10:04 -0700

    if statement notes

---
+++ FirstGame/Assets/KeyRing.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class KeyRing : MonoBehaviour {
+
+	public bool doorLock = true;
+	void OnMouseDown()
+	{
+		if(doorLock == false){
+			print("Door is locked.");
+		}
+		if(doorLock == true)
+		{
+			print("Door is unlocked");
+		}
+	}
+}
--- FirstGame/Assets/stringNotes.cs
+++ FirstGame/Assets/stringNotes.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
 
-public class Animal2 : MonoBehaviour {
+public class stringNotes : MonoBehaviour {
 //strings and ints are variables which is called during functions
 	public string Name; //string is a string of letters, numbers, and characters 
 	public int foodCount; //int is a whole number 
@@ -11,5 +11,25 @@ public class Animal2 : MonoBehaviour {
 	{
 		print("I have a " + Name);//strings can be called and combined with other strings through functions
 		print("It has " + foodCount + " amount of food.");//ints can also be called and combined with strings and other ints through functions
+
+		if (Name == "Dog")
+		{
+			print("I'm loyal");
+		}
+		if(Name == "Cat")
+		{
+			print("Leave me alone.");
+		}
+		if(4 + 5 != 10)
+		{
+			print("No duh.");
+		}
+		if(foodCount < 0){
+			print("I need more food");
+		}
 	}
 }
+//if statements check for true or false outcomes
+//if statements are not called but can do work
+//if statements don't always return something
+//if statements can be used with a bool to turn on true or false for functions and variables.
