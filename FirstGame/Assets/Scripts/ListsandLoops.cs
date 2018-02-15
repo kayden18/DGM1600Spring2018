c4c5b3d9a3ce95feaadc101d5a2eda45b81bbc5e
Unknown <kayden18@ymail.com>
2018-02-15 11:18:19 -0700

    added list scripts

---
+++ FirstGame/Assets/Scripts/ListsandLoops.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ListsandLoops : MonoBehaviour {
+
+	public TheIncredibles TheIncredibles;
+	public Text AddMember; 
+
+	//public string[] PlayerNames;
+	//public List<string> PlayerNameList;
+
+	private void OnMouseDown()
+	{
+		TheIncredibles.FamilyList.Add(AddMember.Text);
+	}
+}
