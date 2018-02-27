98596d15b18a4328044e3534e75fd256256ec2b2
Unknown <kayden18@ymail.com>
2018-02-27 11:16:08 -0700

    forloops

---
+++ FirstGame/Assets/Scripts/ForLoop.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ForLoop : MonoBehaviour {
+
+	public string[] Keys;
+
+	void Start () {
+		for (int i = 0; i < Keys.Length; i++)
+		{
+			if (Keys[i] == "Add Ammo")
+			{
+				print(Keys[i]);
+			}
+		}
+	}
+
+	private void void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space)){
+			Run();
+		}
+	}
+	
+	void Run(){
+		//do work
+	}
+}
+//for is a key word for forloops
+//forloops contain an declaration, ifstatement, and a modifier
+//forloops run until the ifstatement is false
+//void function can be called by any monobehaviour function
