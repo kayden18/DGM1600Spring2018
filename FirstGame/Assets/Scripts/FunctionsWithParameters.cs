6600b603d0ad0d536bdbc053f42908ed14ca6581
Unknown <kayden18@ymail.com>
2018-03-01 20:27:40 -0700

    functions with parameters Homework

--- FirstGame/Assets/Scripts/FunctionsWithParameters.cs
+++ FirstGame/Assets/Scripts/FunctionsWithParameters.cs
@@ -14,20 +14,53 @@ public class FunctionsWithParameters : MonoBehaviour {
 		AddNumbers(14,2);
 		PlayerData("Bob");
 		AddHealth(25);
+		SubHealth(15);
+		AddWeapon("Bow");
+		AddAmmo(15);
+		IncreaseFoodMeter(10);
+		DecreaseFoodMeter(5);
+		AddArmor(25);
+		ArmorDamage(10);
 	}
 
 	void AddNumbers(int a, int b)
 	{
 		print(a += b);
 	}
-
 	void PlayerData(string Name)
 	{
 		print("This player is " + Name);
 	}
-
 	void AddHealth(int NewHealth)
 	{
 		Health += NewHealth;
+	}
+	void SubHealth(int NewHealth)
+	{
+		Health -= NewHealth;
+	}
+	void AddWeapon(string weapon)
+	{
+		print(weapon + " added");
+	}
+	void AddAmmo(int Newammo)
+	{
+		Ammo += Newammo;
+	}
+	void IncreaseFoodMeter(int FoodMeter)
+	{
+		Food += FoodMeter;
+	}
+	void DecreaseFoodMeter(int FoodMeter)
+	{
+		Food -= FoodMeter;
+	}
+	void AddArmor(int ArmorRating)
+	{
+		Armor += ArmorRating;
+	}
+	void ArmorDamage(int ArmorRating)
+	{
+		Armor -= ArmorRating;
 	}
 }
