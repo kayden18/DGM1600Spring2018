cd810bf5b6a949b9387b136d36f3f351bd0ec5df
Unknown <kayden18@ymail.com>
2018-03-29 09:44:54 -0600

    speedscript

---
+++ FirstGame/Assets/CharacterController/SpeedChange.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[CreateAssestMenu]
+
+public class SpeedChange : ScriptableObject {
+    private void OnCollisionEnter(Collision collision)
+        {
+            return float speed = speed++;
+        }
+}
