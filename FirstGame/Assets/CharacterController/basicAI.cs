73db63676f4b13c445d78b9a8f9024b577923be4
Unknown <kayden18@ymail.com>
2018-04-19 10:49:27 -0600

    added basic AI script for enemies

---
+++ FirstGame/Assets/CharacterController/basicAI.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class basicAI : MonoBehaviour {
+
+	public NavMeshAgent Agent;
+	public Transform Player;
+
+	void Start()
+	{
+		Agent = GetComponent<NavMeshAgent>();
+	}
+	void Update()
+	{
+		Agent.destination = Player.position;
+	}
+}
---
+++ FirstGame/Assets/CharacterController/playParticls.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playParticls : MonoBehaviour {
+
+	public ParticleSystem ParticleSystem;
+
+	private void OnTriggerEnter(Collider other)
+	{
+		ParticleSystem.Emit(100);
+	}
+}
